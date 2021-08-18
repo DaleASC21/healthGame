18c8d7fc17f895d9b46c149f13d95e28cdfd6a3e
DaleASC21 <dale21vr@gmail.com>
2021-08-17 19:49:13 -0700

    first commit

--- sketch.js
+++ sketch.js
@@ -1,14 +1,19 @@
+// style
+let body = document.querySelector("body");
+body.style.backgroundColor = "gray";
+
 // game
 let state;
 let score;
+let lives;
 
-// Player
+// player
 let player;
 let playerX;
 let playerY;
 let playerLeft, playerTop, playerRight;
 
-// Good Food
+// good food
 let goodFood;
 let goodFood1;
 let goodFood2;
@@ -20,19 +25,24 @@ let goodFoodX;
 let goodFoodSpeed;
 let goodFoodLeft, goodFoodBot, goodFoodRight;
 
-// Bad Food
+// bad food
 let badFood;
+let badFood1;
+let badFoodArray;
+let randomBadFood;
+let bFood;
 let badFoodY;
 let badFoodX;
 let badFoodSpeed;
 let badFoodLeft, badFoodBot, badFoodRight;
 
 function preload() {
-    player = loadImage("images/test_player.png");
-    goodFood = loadImage("images/blue_sus.png");
-    goodFood1 = loadImage("images/pink_sus.png")
-    goodFood2 = loadImage("images/yellow_sus.png");
-    badFood = loadImage("images/the_imposter.png");
+    player = loadImage("images/pikachu.png");
+    goodFood = loadImage("images/apple.png");
+    goodFood1 = loadImage("images/banana.png")
+    goodFood2 = loadImage("images/blueberry.png");
+    badFood = loadImage("images/fries.png");
+    badFood1 = loadImage("images/burger.png");
 }
 
 function setup() {
@@ -41,8 +51,9 @@ function setup() {
     imageMode(CENTER);
 
     // game
-    state = 0;
     score = 0;
+    lives = 3;
+    state = 0;
 
     // player
     playerY = 700;
@@ -51,20 +62,18 @@ function setup() {
     // good Food
     goodFoodY = -50;
     goodFoodSpeed = random(5, 10);
-    goodFoodSpeed2 = random(5, 10);
-    goodFoodX = random(25, width - 75);
+    goodFoodX = random(75, width - 75);
     randomGoodFood = Math.floor(random(0, 3));
     goodFoodArray = [goodFood, goodFood1, goodFood2];
     Food = goodFoodArray[randomGoodFood];
-    
-    // good Food 2
-    goodFoodX2 = random(25, width - 75);
-    goodFoodY2 = -50;
 
     // bad Food
     badFoodY = -50;
     badFoodSpeed = random(5, 10);
-    badFoodX = random(25, width - 75);
+    badFoodX = random(75, width - 75);
+    randomBadFood = Math.floor(random(0, 2));
+    badFoodArray = [badFood, badFood1];
+    bFood = badFoodArray[randomBadFood];
 }
 
 function draw() {
@@ -74,35 +83,33 @@ function draw() {
     badFoodY += badFoodSpeed;
 
     if (goodFoodY > 900) {
-        randomGoodFood = Math.floor(random(0, 3));
-        goodFoodX = random(75, width - 75);
-        goodFoodY = -50;
-        goodFoodSpeed = random(5, 10);
+        newFood();
     }
 
     if (badFoodY > 900) { 
-        badFoodX = random(75, width - 75);
-        badFoodSpeed = random(5, 10);
-        badFoodY = -50;
+        newBadFood();
     }
 
     Food = goodFoodArray[randomGoodFood];
-
+    bFood = badFoodArray[randomBadFood];
+    
     // draw good food
     image(Food, goodFoodX, goodFoodY, 100, 100);
     
     // draw bad food
-    image(badFood, badFoodX, badFoodY, 100, 100);
+    image(bFood, badFoodX, badFoodY, 100, 100);
 
     // draw player
     image(player, playerX, playerY, 100, 100);
 
     // player movement
-    if (keyIsDown(LEFT_ARROW) && playerX > 75) {
-        playerX -= 7;
-    }
-    if (keyIsDown(RIGHT_ARROW) && playerX < 625) {
-        playerX += 7;
+    if (state == 0) {
+        if (keyIsDown(LEFT_ARROW) && playerX > 75) {
+            playerX -= 7;
+        }
+        if (keyIsDown(RIGHT_ARROW) && playerX < 625) {
+            playerX += 7;
+        }
     }
 
     // player hitbox
@@ -122,21 +129,89 @@ function draw() {
 
     // collision
     if (playerLeft > badFoodRight || playerRight < badFoodLeft || playerTop > badFoodBot) {
+        textSize(30);
+        text("Lives:", 50, 100);
+        textSize(35);
+
+        fill(255, 0, 0);
+        if (lives == 3) {
+            text("♥  ♥  ♥", 150, 100);
+        } 
+        else if (lives == 2) {
+            text("♥  ♥  ", 150, 100);
+        }
+        else if (lives == 1) {
+            text("♥", 150, 100);
+        }
+
         if (playerLeft > goodFoodRight || playerRight < goodFoodLeft || playerTop > goodFoodBot) {
+            textSize(30);
             fill(255);
-            text(score, 255, 50);
+            text("Score:", 470, 100);
+            text(score + "  / 10", 570, 100);
         }
         else {
             score++;
-            goodFoodY = -50;
-            goodFoodX = random(75, width - 75);
-            randomGoodFood = Math.floor(random(0, 3));
-
+            newFood();
+            textSize(30);
             fill(255);
-            text(score, 255, 50);
+            text(score + "  / 10", 570, 100);
         }
     }
     else {
+        lives--;
+        console.log(lives);
+        newBadFood();
+    }
+
+    if (lives == -1) {
+        lose();
+    }
+
+    if (score == 10) {
+        win();
+    }
+}
+
+function mouseClicked() {
+    if (state == 1) {
         setup();
     }
+}
+
+// personal functions
+function newFood() {
+    goodFoodY = -50;
+    goodFoodX = random(75, width - 75);
+    randomGoodFood = Math.floor(random(0, 3));
+    goodFoodSpeed = random(5, 10);
+}
+
+function newBadFood() {
+    badFoodY = -50;
+    badFoodX = random(75, width - 75);
+    randomBadFood = Math.floor(random(0, 2));
+    badFoodSpeed = random(5, 10);
+}
+
+function lose() {
+    state = 1;
+    goodFoodSpeed = 0;
+    badFoodSpeed = 0;
+    textSize(50);
+    fill(255);
+    text("You Lose!", 245, 425);
+    textSize(20);
+    text("Click anywhere to try again!", 235, 485);
+}
+
+function win() {
+    state = 1;
+    goodFoodSpeed = 0;
+    badFoodSpeed = 0;
+    textSize(50);
+    fill(255);
+    text("You Win!", 255, 425);
+    textSize(20);
+    text("Click anywhere to try again!", 235, 485);
 }
